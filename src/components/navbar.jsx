ad3bd3222dd5e2fae0ce87b2be127250c3960131
LCSS <respons29@gmail.com>
2020-09-03 20:20:41 +0200

    made NavBar stateless component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( <nav className="navbar navbar-dark bg-dark">
+//stateless Functional Component
+
+const NavBar = () => {
+  return ( <nav className="navbar navbar-dark bg-dark">
         <span className="navbar-brand mb-0 h1">Fortune Teller</span>
       </nav> );
-    }
 }
  
 export default NavBar;
